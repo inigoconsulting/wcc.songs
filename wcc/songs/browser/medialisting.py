2e013f318d977c36958e522ed026c67399bcc525
kulll <anwarbaik88@gmail.com>
2012-11-27 16:46:33 +0800

    wcc.songs:     * add collection support for media listing re: #80

--- wcc/songs/browser/medialisting.py
+++ wcc/songs/browser/medialisting.py
@@ -5,6 +5,7 @@ from hurry.filesize import alternative
 from Acquisition import aq_inner
 import os
 from Products.ATContentTypes.interfaces.file import IATFile
+from plone.app.collection.interfaces import ICollection
 from zope.interface import Interface
 from wcc.songs.song import ISong
 
@@ -46,6 +47,11 @@ class MediaListing(grok.View):
         except:
             return False
         return True
+
+
+class CollectionMediaListing(MediaListing):
+    grok.context(ICollection)
+
 
 
 class FileAdapter(grok.Adapter):
