1c14d16c500daec551b445935e34a9da1305ae4a
Izhar Firdaus <izhar@inigo-tech.com>
2012-12-11 01:28:01 +0800

    - make song to be language independent (re #104) - add other dublincore fields to songs (re #103)

--- wcc/songs/song.py
+++ wcc/songs/song.py
@@ -24,10 +24,13 @@ from Products.ATContentTypes.interfaces.folder import IATFolder
 from hurry.filesize import size as readable_size
 from hurry.filesize import alternative
 from plone.app.collection.interfaces import ICollection
+from plone.multilingualbehavior import directives as pam
+from plone.app.dexterity.behaviors.metadata import IBasic
 
+from plone.app.dexterity import PloneMessageFactory as _d
 # Interface class; used to define content-type schema.
 
-class ISong(form.Schema, IImageScaleTraversable):
+class ISong(form.Schema, IImageScaleTraversable, IBasic):
     """
     Manage Song
     """
@@ -38,24 +41,43 @@ class ISong(form.Schema, IImageScaleTraversable):
     # models/song.xml to define the content type
     # and add directives here as necessary.
 
+    pam.languageindependent('title')
+    title = schema.TextLine(
+        title = _d(u'label_title', default=u'Title'),
+        required = True
+        )
+
+    pam.languageindependent('description')
+    description = schema.Text(
+        title=_d(u'label_description', default=u'Description'),
+        description = _d(u'help_description', default=u'A short summary of the content.'),
+        required = False,
+        missing_value = u'',
+        )
+
+    pam.languageindependent('audio_file')
     audio_file = NamedBlobFile(
             title=_(u'Audio'),
             description=_('Upload the MP3 or AAC audio file here'),
             required=True,
     )
 
+    pam.languageindependent('score_file')
     score_file = NamedBlobFile(
             title=_(u'Score (PDF)'),
             description=_(u'Upload score PDF of the song here'),
             required=True,
     )
 
+    pam.languageindependent('score_image_file')
     score_image_file = NamedBlobFile(
             title=_(u'Score (TIFF)'),
             description=_(u'Upload score TIFF of the song here'),
             required=True,
     )
 
+    form.order_before(description = '*')
+    form.order_before(title = '*')
 
 # View class
 # The view will automatically use a similarly named template in
