8ece61c90561d6fbef979fb1d322fd37814b7182
Izhar Firdaus <izhar@inigo-tech.com>
2012-12-17 14:52:31 +0800

    add upgrade profile to 1002

--- wcc/songs/__init__.py
+++ wcc/songs/__init__.py
@@ -2,3 +2,16 @@ from zope.i18nmessageid import MessageFactory
 
 # Set up the i18n message factory for our package
 MessageFactory = MessageFactory('wcc.songs')
+
+from Products.CMFQuickInstallerTool.interfaces import INonInstallable
+from zope.interface import implements
+
+class HiddenProducts(object):
+    """This hides the upgrade profiles from the quick installer tool."""
+    implements(INonInstallable)
+
+    def getNonInstallableProducts(self):
+        return [
+            'wcc.songs.upgrades'
+        ]
+
--- wcc/songs/upgrades/handlers.py
+++ wcc/songs/upgrades/handlers.py
@@ -4,3 +4,7 @@ def to1001(context):
     setup = getToolByName(context, 'portal_setup')
     setup.runAllImportStepsFromProfile('profile-wcc.songs.upgrades:to1001')
 
+def to1002(context):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runAllImportStepsFromProfile('profile-wcc.songs.upgrades:to1002')
+
